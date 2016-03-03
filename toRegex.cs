ea8858003f977733185301c83a074e826d67e405
Manas Rawat <manas.rawat2002@gmail.com>
2016-03-03 19:31:32 +0000

    Update toRegex.cs

--- toRegex.cs
+++ toRegex.cs
@@ -2,48 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 	
-namespace cyource_console_project 
+namespace exclusiveToRegex
 {
 
-	class toRegex
+	class Program
 	{
-			static void Main(string[]args)
-			{
-				Console.WriteLine("a");
-				string input = Console.ReadLine();
-				List<char>chars = new List<char>();
+		static void Main(string[]args)
+		{
+
+			string input;
+			while (true) {
+			input = Console.ReadLine();
+			List<char>chars = new List<char>();
 		
-				foreach (char c in input){
-					chars.Add(c);
-				}
+			foreach (char c in input){
+				chars.Add(c);
+			}
 				
-				for (int i = 0; i < chars.Count; i++) {
-					//insert @"\w" if you must, but what's the point? It'll be pretty useless regex then.
-					char[]regexes = {'d','s'};
-					string[] regexes2 = {@"\d",@"\s"};
-					for (int k = 0; k <regexes.Length;k++) {
-						if (Regex.IsMatch(chars[i].ToString(), regexes2[k])){
-							chars.Remove(chars[i]);
-							chars.Insert(i, '\\');
-							chars.Insert(i+1,regexes[k]);
-						} else if (j>0 && j <= chars.Count) {
-							chars.Insert(chars[j-1], '\');
+			for (int i = 0; i < chars.Count; i++) {
+				char[]regexes = {'d','s'};
+				string[] regexes2 = {@"\d",@"\s"};
+					
+				for (int k = 0; k < regexes.Length;k++) {
+					if (Regex.IsMatch(chars[i].ToString(), regexes2[k])){
+						chars.Remove(chars[i]);
+						chars.Insert(i, '\\');
+						chars.Insert(i+1,regexes[k]);
+						
+						if (i+3 < chars.Count && Regex.IsMatch(chars[i+2].ToString(), @"\w") 
+						&& Regex.IsMatch(chars[i+2].ToString(), @"\d|\s").Equals(false)){
+							chars.Insert(i+2, '\\');
+							chars.Insert(i+3, '\\');
 						}
-					}
-				}
-				
-				for (int j = 0; j< chars.Count; j++){
-						if (j>0 && j < chars.Count && chars[j].Equals(chars[j+2]) 
-						&& chars[j+1].Equals(chars[j+3])) {
-							chars.Remove(chars[j+2]);
-							chars.Remove(chars[j+3]);
-							chars.Insert(j+2,'+');
+							
+						/*if (i+3 <= chars.Count && chars[i] == '\\' && chars[i+1] == 'd' && chars[i]==chars[i+2] && chars[i+1]==chars[i+3]) {
+							chars.Remove(chars[i+2]);
+							chars.Remove(chars[i+3]);
+							chars.Insert(i+2,'+');
+						}*/
 						} 
-				}
+						
+					}
+						
+				}	
+				
+				//string[] toShow = {string.Join("",chars.ToArray()), "\\\\"+ input};	
 				
 				Console.WriteLine("@\"" + string.Join("",chars.ToArray()) + "\"");
+
 		
 			}
-
+		}
 	}
 }
