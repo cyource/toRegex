10f54f38443b8276376ac735c9e3e60a965cd7c4
Manas Rawat <manas.rawat2002@gmail.com>
2016-03-07 18:41:11 +0000

    Update toRegex.cs

--- toRegex.cs
+++ toRegex.cs
@@ -5,73 +5,74 @@ using System.Text.RegularExpressions;
 namespace toRegex
 {
 
-	class Program
+	public class Program
 	{
-			static void Main(string[]args)
+			public static void Main(string[]args)
 			{
 
 				string input;
 				input = Console.ReadLine();
+				
 				List<char>chars = new List<char>();
-				List<char>chars2 = new List<char>();
-				List<string>strings = new List<string>();
-				List<string>strings2 = new List<string>();
+				
+				List<List<string>> listslist = new List<List<string>>();
 		
 				foreach (char c in input){
 					chars.Add(c);
-					chars2.Add(c);
 				}
 				
 				for (int i = 0; i < chars.Count; i++) {
 					char[]regexes = {'d','s'};
 					string[] regexes2 = {@"\d",@"\s"};
 					
-					for (int j = 0; j < 2;j++) { //k
-						if (Regex.IsMatch(chars[i].ToString(), regexes2[j])){ //-2
+					for (int j = 0; j < 2;j++) { //1s
+						if (Regex.IsMatch(chars[i].ToString(), regexes2[j])){ //2s
+							
+							List<string>strings = new List<string>();
+							List<string>strings2 = new List<string>();
 							
 							string abc = "abcdefghijklmnopqrstuzwxyz";
 							
 							List<char>xyz = new List<char>();
-							foreach (char a in abc){ //-1
+							foreach (char a in abc){ //3s
 								xyz.Add(a);
-							} //-1
+							} //3e
 							
-							string chara = string.Join("",chars2.ToArray()).ToLower();
+							string chara = (string.Join("",chars.ToArray()).ToLower());
 							int count = chara.Split(' ').Length - 1;
 							
-							for (int l = 0; l <= count; l++) { //0
+							for (int l = 0; l <= count; l++) { //4s
 								
 								List<char>mn = new List<char>();
 								List<char>nm = new List<char>();
 							
-								string[] split = chara.Split(' ');		
+								string[] split = Regex.Split(chara,@"(?<=\s)");		
 								string splitee = split[l];
 								
 								List<char> splits = new List<char>();
 								foreach (char s in splitee){
 									splits.Add(s);
-								} //0
+								} 
 							
-							for (int k = 0; k < 26; k++){ //1
+							for (int k = 0; k < 26; k++){ //5s
 								if (splits.Contains(xyz[k])){
 									mn.Add(xyz[k]);
 								} else if (splits.Contains(xyz[k]).Equals(false)){
 									nm.Add(xyz[k]);
 								} 
-							}	//1
+							}	//5e
 								
 							String accepted = "[" + string.Join("",mn.ToArray()) + "]";
 							String rejected = "[^" + string.Join("",nm.ToArray()) + "]";
 
-							String ar = Regex.Replace(string.Join("",chars.ToArray()).ToLower(),@"[a-z]","");
-							String r = Regex.Replace(string.Join("",chars.ToArray()).ToLower(),@"[a-z]","");
+							String ar = Regex.Replace(string.Join("",splits.ToArray()).ToLower(),@"[a-z]","");
 								
 							strings.Add(accepted + ar);
-							strings2.Add(rejected + r);
-							} //-2
-								//here	
-							Console.WriteLine(string.Join("",strings.ToArray()));
-							Console.WriteLine(string.Join("",strings2.ToArray()));
+							strings2.Add(rejected + ar);
+								
+							listslist.Add(strings);
+							listslist.Add(strings2);
+							} //2e
 							
 							chars.Remove(chars[i]);
 							chars.Insert(i, '\\');
@@ -82,14 +83,13 @@ namespace toRegex
 								chars.Insert(i+2, '\\');
 								chars.Insert(i+3, '\\');
 							}
-						} //k
+						} //4e
 						
-					}
+					} //1e
 						
 				}	
-					//Console.WriteLine(string.Join("",strings.ToArray()));
-					//Console.WriteLine(string.Join("",strings2.ToArray()));
-					
+					Console.WriteLine(string.Join("",listslist[0].ToArray()));
+					Console.WriteLine(string.Join("",listslist[1].ToArray()));
 					Console.WriteLine("@\"" + string.Join("",chars.ToArray()) + "\"");
 
 		
